c591615288a0431f75fca77a2c749bf895e6fe07
Mien Thai-Thanh <mthaithanh@protonmail.com>
2022-10-06 20:22:06 +0200

    Meaningful commit message

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+if dnf list --installed ansible &> /dev/null; then
+    echo "ansible is already installed"
+else
+    sudo dnf install ansible --assumeyes
+fi
+
 ansible-playbook \
     -i hosts \
-    -K playbook.yml 
+    -K playbook.yml
